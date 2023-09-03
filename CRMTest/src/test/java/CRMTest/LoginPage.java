86e2f42b43b11049996ac666ad9bc10395898038
Michał Łysiński <128403354+Lysy-M@users.noreply.github.com>
2023-09-03 21:07:56 +0200

    Update LoginPage.java

--- CRMTest/src/test/java/CRMTest/LoginPage.java
+++ CRMTest/src/test/java/CRMTest/LoginPage.java
@@ -10,12 +10,12 @@ public class LoginPage {
     public LoginPage(WebDriver driver) {this.driver = driver; }
 
     public void loginAs(String email, String password) {
-        WebElement loginInput = driver.findElement(By.id("user_name")); //mlysinski@wp.pl
+        WebElement loginInput = driver.findElement(By.id("user_name")); 
         loginInput.click();
         loginInput.clear();
         loginInput.sendKeys(email);
 
-        WebElement passwordInput = driver.findElement(By.id("username_password")); //Likwidator25
+        WebElement passwordInput = driver.findElement(By.id("username_password")); 
         passwordInput.click();
         passwordInput.clear();
         passwordInput.sendKeys(password);
